cbd3eda52efbb1d6e961f192616620f961596d52
Denis.Torgov <deniska-3000@bk.ru>
2023-07-12 06:30:30 +0300

    sumstr added

--- Sem2/program.java
+++ Sem2/program.java
@@ -38,6 +38,17 @@ public class program_2_4 {
         return first + second;
     }
 
+    private static double substr (int x) {
+        System.out.println("Введите первое число");
+        Scanner iScanner = new Scanner(System.in);
+        double first = Double.parseDouble(iScanner.nextLine());
+        System.out.print("-");
+        double second = Double.parseDouble(iScanner.nextLine());
+        iScanner.close();
+        return first - second;
+    }
+
+
     public static void main(String[] args)  throws IOException {
         Logger logger = Logger.getLogger(program_2_4.class.getName());
         FileHandler fh = new FileHandler("seminar_2/log_program_4_.txt");
