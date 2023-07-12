c613cffafbd7600bbe90facfd5c2e05b08474cbe
Denis.Torgov <deniska-3000@bk.ru>
2023-07-12 06:28:06 +0300

    start

---
+++ Sem2/program.java
@@ -0,0 +1,43 @@
+// 4*. Реализуйте простой калькулятор, с консольным интерфейсом.
+// К калькулятору добавить логирование.
+
+
+package Sem2;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.*;
+
+
+public class program_2_4 {
+    private static String messages (int x) {
+        System.out.println("Выбирете желаемое действие:\n 1 - Сумма двух чисел\n 2 - Разность двух чисел\n 3 - Произведение двух чисел\n 4 - Производное двух чисел");
+        Scanner iScanner = new Scanner(System.in);
+        int action = Integer.parseInt(iScanner.nextLine());
+        if (action > 4 || action <= 0) {
+            return messages(0);
+        }
+        return doit(action);
+    }
+    private static String doit (int x) {
+        String result = "";
+        if (x == 1) result += sum(0);
+        else if (x == 2) result += substr(0);
+        else if (x == 3) result += multi(0);
+        else if ( x == 4) result += divis(0);
+        return result;
+    }
+        
+    public static void main(String[] args)  throws IOException {
+        Logger logger = Logger.getLogger(program_2_4.class.getName());
+        FileHandler fh = new FileHandler("seminar_2/log_program_4_.txt");
+        SimpleFormatter sFormat = new SimpleFormatter();
+        logger.addHandler(fh);
+        fh.setFormatter(sFormat);
+        // System.out.println(messages(0));
+        logger.info(messages(0));
+
+
+
+    }
+}
